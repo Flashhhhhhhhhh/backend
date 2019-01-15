7320a3db747dd0df4f5a9be93857ad0dca9b170f
Victoria Law <vhlaw@calpoly.edu>
2019-01-15 10:27:53 -0800

    count uniqueness

--- csvToJson.py
+++ csvToJson.py
@@ -2,6 +2,7 @@
 
 import csv
 import json
+import pandas as pd
 
 csvFilePath = "data.csv"
 jsonFilePath = "happy.json"
@@ -23,3 +24,20 @@ root["id"] = data
 
 with open(jsonFilePath, "w") as jsonFile:
    jsonFile.write(json.dumps(root, indent=4))
+
+# count the variation within each column/category
+df = pd.read_csv('data.csv')
+list1 = pd.read_csv('data.csv', nrows=1).columns.tolist()
+list2 = []
+count = 0
+for thing in list1:
+   for row in df[thing].unique():
+      count = count + 1
+   list2.append(count)
+   count = 0
+
+for thing in list1:
+   print(thing)
+
+for num in list2:
+   print(num)
