0da3ceea95f68a17d77047e6623c4dc76fe7ae7d
bogalvan <bogalvan6@gmail.com>
2019-01-29 17:56:23 +0000

    parsed temp data file

--- csvToJson.py
+++ csvToJson.py
@@ -4,40 +4,79 @@ import csv
 import json
 import pandas as pd
 
-csvFilePath = "data.csv"
-jsonFilePath = "happy.json"
+combined_headers = {}
 
-# Read the CSV and add the data to a dictionary...
-data = {}
-with open(csvFilePath) as csvFile:
-   csvReader = csv.DictReader(csvFile)
-   for csvRow in csvReader:
-      id = csvRow["id"]
-      data[id] = csvRow
+class CsvInfo:                                     #Object that holds the filename(lineage?) and 10 examples for a given header 
+   def __init__(self, file_name):
+      self.file_name = file_name
+      self.examples = []    
 
-#print(data)
-# Add data to a root node...
-root = {}
-root["id"] = data
+   def add_example(self, example):
+      self.examples.append(example)
 
-# Write data to a JSON file
 
-with open(jsonFilePath, "w") as jsonFile:
-   jsonFile.write(json.dumps(root, indent=4))
+class Header:                                     #Object to represent a column header incldudes header name, list of cvs that included this header, and a tag 
 
-# count the variation within each column/category
-df = pd.read_csv('data.csv')
-list1 = pd.read_csv('data.csv', nrows=1).columns.tolist()
-list2 = []
-count = 0
-for thing in list1:
-   for row in df[thing].unique():
-      count = count + 1
-   list2.append(count)
-   count = 0
+   def __init__(self, name):
+      self.name = name
+      self.csv_files = []
+      self.tag = []
 
-for thing in list1:
-   print(thing)
 
-for num in list2:
-   print(num)
+   def add_csv_file(self, csv_file):              #adds a csv file that includes this header 
+      self.csv_files.append(csv_file)
+
+   def print_header(self):                        #IN PROGRESS prints this header in json format 
+      start = '   "%s": {\n'%(self.name)
+      examples_line = "   "
+      for file in self.csv_files:                  #this will only be one on the first round 
+         file_name_line = '      "%s":' %(file.file_name)
+         examples_line = examples_line + "["
+         for examples in file.examples:
+            example_string = '"%s", ' %(examples)
+            examples_line = examples_line + example_string
+         examples_line = examples_line + "null],\n"
+         full_file_info = file_name_line + examples_line
+      tag = '      "tag": [null],\n'
+      end = "   }"
+      final_line = start +full_file_info+ tag + end
+      return final_line
+
+
+def readFile(csv_file_path):                            #meant to take in a cvs path (using a temp currently) in order to read all column headers and include first 10 examples from each
+   df = pd.read_csv(csv_file_path)
+   columns = list(df.head(0))                          #gets list of all coloumn headers 
+   for columnHeader in columns:                        #goes through each column header in csv   
+         columnHeader_temp = Header(columnHeader)      #creates a header object for each column in csv 
+         #print(columnHeader);
+         item_in_c = df[0:10][columnHeader]            #array of first 10 examples in column 
+         csv_temp = CsvInfo(csv_file_path) 
+         for item in item_in_c:                        #for each of ten examples add them to that csv file info object 
+            csv_temp.add_example(item) 
+            #print(item);
+         columnHeader_temp.add_csv_file(csv_temp);     #add csv examples to header object 
+         combined_headers[columnHeader] = columnHeader_temp #add header object to global group of headers 
+
+
+
+
+
+def main():
+   readFile('dataTemp.csv') 
+   #readFile('dataTemp2.csv')
+   with open('temp.json', 'w') as f:
+      f.write("{\n")
+      for header_num in range(len(combined_headers)):
+         header = combined_headers.get(combined_headers.keys()[header_num])
+         line = header.print_header()
+         if header_num != len(combined_headers) - 1:
+            line = line + ","
+            f.write(line + "\n")
+      f.write("}")
+
+if __name__ == '__main__':
+    main()
+
+
+
+
