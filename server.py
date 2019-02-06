53cc73674de0e4580dcde23e5994c44ea50d9499
Victoria Law <lawhvictoria@gmail.com>
2019-02-06 07:45:01 +0000

    upload endpoints

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS
+from flask import requests 
+from csvToJson.py import readFile
+import werkzeug
 
 app = Flask(__name__)
 CORS(app)
@@ -26,5 +29,15 @@ class TestData(Resource):
             return datum, 200
       return "Test data not found", 404
 
+class FileUpload(filename):
+   # sending file to backend from frontend   
+   def post(self):
+      print request.get_data()
+      csv_data_array = request.form['file']
+      csv_file = csv_data_array[0]
+      convertedFile = readFile(csvFile)
+      return convertedFile, 200
+
 api.add_resource(TestData, "/test/<string:name>")
+api.add_resource(FileUpload, "/upload")
 app.run(debug=True,host='0.0.0.0')
