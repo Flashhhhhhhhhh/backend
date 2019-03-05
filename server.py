cd569cb6275b867347649dd7d60cfffcb14c75fd
Victoria Law <lawhvictoria@gmail.com>
2019-03-05 17:49:20 +0000

    Endpoints

---
+++ server.py
@@ -0,0 +1,53 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS
+from flask import request
+from csvToJson import readFile
+import werkzeug
+
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
+testData = [
+   {
+      "name" : "Barry Allen",
+      "age" : 21,
+      "occupation" : "Hosptial Analyst"
+   },
+   {
+      "name" : "test",
+      "age" : 55,
+      "occupation" : "Thing Doer"
+   },
+]
+
+class TestData(Resource):
+   def get(self, name):
+      for datum in testData:
+         if (name == datum["name"]):
+            return datum, 200
+      return "Test data not found", 404
+
+class FileUpload(Resource):
+   # sending file to backend from frontend
+   def post(self):
+      f = request.files['file'] #request the csv file
+      csv_data_array = request.form['file'] # get the file array
+      csv_file = csv_data_array[0] # get the specific csv file
+      convertedFile = readFile(csv_file) #run the algorithm
+      return convertedFile, 200 #return
+
+class UpdateFinal(Resource):
+   # final upload of JSON with tag system
+   def post(self):
+      f = request.files['file'] #request the JSON file
+      json_data_array = request.form['file'] # get the file array
+      json_file = json_data_array[0] # get the specific JSON file
+      convertedFile = json_file 
+      return convertedFile, 200 # return
+
+api.add_resource(TestData, "/test/<string:name>")
+api.add_resource(FileUpload, "/upload")
+api.add_resource(UpdateFinal, "/updateFinal")
+app.run(debug=True,host='0.0.0.0',port=5002)
