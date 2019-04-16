a9d5fd74ac3953448bbfafae09c4dc7ba101e6ba
jbuelow <joseph.buelow@yahoo.com>
2019-04-16 17:25:03 +0000

    updated TrainingDataMaker to take in and out files to update model based on user changes

--- src/TrainingDataMaker.py
+++ src/TrainingDataMaker.py
@@ -2,35 +2,37 @@
 
 import json
 
+def appendMove(out_json, move):
+   tag_string = "{\"tags\": [\"" + move[0] + "\"]}"
+   tag_json = json.loads(tag_string)
+   out_json[move[1]][move[0]] = tag_json
+   return out_json
 
-def move(tag):
-   #print tag
+def getMove(tag):
    for field in tag:
       if field == 'dest':
          dest = tag[field]
       elif field == 'src':
          src = tag[field]
-   print "(\""+src+"\", {\'tags\': [\'"+dest+"\']})"
+   return (src.upper(), dest.upper())
 
+def makeTrainingData(in_file, out_file):
+   changeFile = open(in_file)
+   out = open(out_file)
+   changes = json.load(changeFile)
+   training = json.load(out)
 
-def parseChange(change):
-   #print change
-   for tag in change:
-      if tag != None:
-         if tag == 'move':
-            move(change[tag])
-         #else:
-         #   print 'no such tag'
-         #   print tag
-   #print change['tag']
+   for obj in changes:
+      for tag in changes[obj]['tag']:
+         if tag != None and tag == 'move':
+            move = getMove(changes[obj]['tag'][tag])
+            training = appendMove(training, move)
+   out.close()
+   out = open(out_file, 'w')
+   json.dump(training, out)
 
-def main():
-  changeFile = open("headers.json")
-  tagMap = open("tagMap.json")
-  changes = json.load(changeFile)
-  #tags = json.load(tagMap)
-  for obj in changes:
-     parseChange(changes[obj]['tag'])
-
-if __name__ == "__main__":
-   main()
+#def main():
+#   makeTrainingData("myHeaders.json", "my_training_data.json")
+#
+#if __name__ == "__main__":
+#   main()
