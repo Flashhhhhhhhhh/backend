52282b9eefb13d160bb7ec8acf14ecd90b997879
root <root@ip-172-31-90-160.ec2.internal>
2019-04-25 17:09:54 +0000

    adjusting paths for dev

--- FlashUpdate.sh
+++ FlashUpdate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd /Flash/backend
+cd /Flash-dev/backend
 gitResults=`git pull`
 
 if [[ $gitResults == "Already up to date." ]] ; then
    exit 0
 else 
-   eval systemctl restart FlashCapstone > /dev/null
+   eval systemctl restart FlashCapstone-dev > /dev/null
    exit 2
 fi
