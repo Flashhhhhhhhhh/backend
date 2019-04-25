98fbf9f7868bfd83591a87f9b99110012691a0b8
root <root@ip-172-31-90-160.ec2.internal>
2019-04-25 16:45:36 +0000

    adding restart script

---
+++ FlashUpdate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /Flash/backend
+gitResults=`git pull`
+
+if [[ $gitResults == "Already up to date." ]] ; then
+   exit 0
+else 
+   eval systemctl restart FlashCapstone > /dev/null
+   exit 2
+fi
