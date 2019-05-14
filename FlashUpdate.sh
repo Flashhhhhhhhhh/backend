e5051cd4b71b4a11314bb53818111bcb805fa517
jbuelow <joseph.buelow@yahoo.com>
2019-05-14 04:17:32 +0000

    added email functionality on failure

--- FlashUpdate.sh
+++ FlashUpdate.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 gitResults=`git pull`
+if [[ $? != 0 ]] ; then
+   echo "System attemped a 'git pull' of backend that failed" | mail -s "Failed to update backend" joseph.buelow@yahoo.com
+   exit 1
+fi
 
 if [[ $gitResults == "Already up to date." ]] ; then
    exit 0
 else 
    eval systemctl restart FlashCapstone > /dev/null
-   exit 2
+   if [[ $? != 0 ]] ; then
+      echo "System attempted to restart backend and failed" | mail -s "Failed to restart FlashCapstone" joseph.buelow@yahoo.com
+      exit 1
+   fi
+   exit 0
 fi
