a183ac85b57a152c4d0644e991544cd20ea99fc2
bogalvan <bogalvan6@gmail.com>
2019-05-22 18:44:02 -0700

    Removed Excess Print Statements

--- src/MachineLearning/OntologyToTagMapGenerator.py
+++ src/MachineLearning/OntologyToTagMapGenerator.py
@@ -8,9 +8,6 @@ def create_tag_map(ont_list):
    #needs path to directory in S3 called "MLfiles"
    tag_map_dir = '/Flash/tag_map.json'
 
-   #print("tag_map")
-   #print(ont_list)   
-
    if not ont_list:
       for filename in os.listdir(ontology_dir):
          with open((ontology_dir+"/"+filename)) as ont_tag_map:
@@ -21,9 +18,6 @@ def create_tag_map(ont_list):
       make_tag_map(j,tag_map_dir)
 
 def make_tag_map(loaded_json,output_dir):
-   #with open(ont_path) as ont_tag_map:
-      #loaded_json = json.load(ont_tag_map)
-   #print(loaded_json)
    with open(output_dir,'w') as tag_map:
       tag_map.write("{\n")
       for header in loaded_json:
